2be69e6c880c289baf8778d24489987133ca443e
VladRadigin <angry.gamer.comi@gmail.com>
2023-07-10 02:23:43 +0300

    Registration of the model in the admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author') # <- перечисляем поля, которые
+    # должны отображаться в админке.
+    # pk => уникальный идентификатор записи в базе данных (будет выводиться, если
+    # тело поста окажется пустым).
+    search_fields = ('text',) # <- отображаем интерфейс для поиска по тексту постов
+    list_filter = ('pub_date',) # <- добавляем возможность фильтрации по дате
+    empty_value_display = '-пусто-' # <- для отображение пустых полей можно установить
+    # значение по умолчанию, и теперь если тело поста окажется пустым, будет выводиться
+    # это значение.
+
+
+admin.site.register(Post, PostAdmin) # <- регистрируем модели в админке

239eb94aa22db013a6a8b98d68f6810c6feed06e
VladRadigin <angry.gamer.comi@gmail.com>
2023-07-10 01:45:35 +0300

    Created model posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Post(models.Model):
+    text = models.TextField() # поле для хранения произвольного текста;
+    pub_date = models.DateTimeField('date published', auto_now_add=True) # поле для хранения даты и времени;
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts') # поле, 
+    # в котором указывается ссылка на другую модель; 
+    # (ссылка на другую таблицу), Параметр on_delete=models.CASCADE обеспечивает связность данных: если из 
+    # таблицы User будет удалён пользователь, то будут удалены все связанные с ним посты.

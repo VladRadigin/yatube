0f36cdb1af9eb11487cd2adf392fbd9e82d6a842
VladRadigin <angry.gamer.comi@gmail.com>
2023-07-10 05:41:13 +0300

    Adding a home page

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    latest = Post.objects.order_by('-pub_date')[:10] # <- получаем все записи из базы
+    output = []
+    # собираем тексты постов в один, разделяя новой строкой:
+    for item in latest:
+        output.append(item.text)
+    return HttpResponse('\n'.join(output))
